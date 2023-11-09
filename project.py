1d9b1600e24dbc89233807e112eeb7deeff8f113
Sharmila-1907 <146049137+Sharmila-1907@users.noreply.github.com>
2023-11-09 10:43:10 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import random
 
-class NumberGuessingGame:
+class guessing_game:
     def __init__(self, min_number, max_number):
         self.min_number = min_number
         self.max_number = max_number
@@ -9,8 +9,7 @@ class NumberGuessingGame:
 
     def play(self):
         print("Welcome to the Number Guessing Game!")
-        print(f"I'm thinking of a number between {self.min_number} and {self.max_number}.")
-
+       
         while True:
             try:
                 # Get the player's guess
@@ -19,15 +18,15 @@ class NumberGuessingGame:
 
                 # Check if the guess is correct
                 if player_guess == self.secret_number:
-                    print(f"Congratulations! You guessed the number {self.secret_number} in {self.attempts} attempts.")
+                    print(f"Congratulations! You have guessed the number correctly  {self.secret_number} in {self.attempts} attempts.")
                     break
                 elif player_guess < self.secret_number:
                     print("Try a higher number.")
                 else:
                     print("Try a lower number.")
             except ValueError:
-                print("Please enter a valid number.")
+                print("Please enter a valid number!!.")
 
 if __name__ == "__main__":
-    game = NumberGuessingGame(1, 100)  # Set the range for the game
-    game.play()
+    game = guessing_game(1,50)  # Setting the range for the game
+    game.play() #calling the play function 
